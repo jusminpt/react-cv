c81eb4b764f077632530a7f62a1af69334f3d119
jmjusmin <jmjusmin1412@gmail.com>
2022-11-10 23:10:19 -0800

    Implement the about section

--- frontendcv/src/pages/About/About.jsx
+++ frontendcv/src/pages/About/About.jsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import "./About.scss";
+import { images } from "../../constants";
+import { urlFor, client } from "../../client";
 
 function About() {
-  return <div>About</div>;
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    //fecth info
+    const fecthData = async () => {
+      const query = '*[_type == "abouts"]';
+      const data = await client.fetch(query);
+
+      setAbouts(data);
+    };
+
+    fecthData();
+  }, []);
+
+  return (
+    <div className="app__about">
+      <h2 className="head-text">
+        I know that <span>Good Dev</span>
+        <br />
+        means <span>Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default About;
